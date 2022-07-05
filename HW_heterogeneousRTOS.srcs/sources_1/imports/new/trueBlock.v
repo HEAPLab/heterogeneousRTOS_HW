b7f0e71be7bc572be6e3a3dd93cc404b2ece08aa
Francesco Ratti <francesco.ratti98@gmail.com>
2022-07-05 22:21:00 +0200

    replaced bmem generator ip with custom component scheduler which includes block ram (or distributed ram if the appropriate pragma is specified). The scheduler component will be in charge of scheduling. DMA WRITES DIRECTLY INTO THE SCHEDULER

---
+++ HW_heterogeneousRTOS.srcs/sources_1/imports/new/trueBlock.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/14/2022 04:34:35 PM
+// Design Name: 
+// Module Name: trueBlock
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module trueBlock(
+    output y
+    );
+    assign y=1;
+endmodule
---
+++ HW_heterogeneousRTOS.srcs/sources_1/new/scheduler.v
@@ -0,0 +1,143 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/05/2022 06:16:51 PM
+// Design Name: 
+// Module Name: scheduler
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module scheduler(
+    start,
+    aresetn,
+    addra,
+    dina,
+    clka,
+    wea,
+    ena,
+    rsta,
+    regcea,
+    douta,
+    started
+    );    
+    
+    //  Xilinx Single Port No Change RAM
+  //  This code implements a parameterizable single-port no-change memory where when data is written
+  //  to the memory, the output remains unchanged.  This is the most power efficient write mode.
+  //  If a reset or enable is not necessary, it may be tied off or removed from the code.
+
+//32k ram
+  parameter RAM_WIDTH = 32;                  // Specify RAM data width
+  parameter RAM_DEPTH = 8192;                  // Specify RAM depth (number of entries)
+  parameter RAM_PERFORMANCE = "HIGH_PERFORMANCE"; // Select "HIGH_PERFORMANCE" or "LOW_LATENCY" 
+  parameter INIT_FILE = "";                       // Specify name/location of RAM initialization file if using one (leave blank if not)
+
+  //  The following function calculates the address width based on specified RAM depth
+  /*
+  function integer clogb2;
+    input integer depth;
+      for (clogb2=0; depth>0; clogb2=clogb2+1)
+        depth = depth >> 1;
+  endfunction */
+ 
+   input start;
+   //input [clogb2(RAM_DEPTH-1)-1:0] addra;
+   input [14:0] addra;
+   input [RAM_WIDTH-1:0] dina;
+   input clka;
+   //input wea;
+   input [3:0] wea;
+   input ena;
+   input rsta;
+   input regcea;
+   output [RAM_WIDTH-1:0] douta;
+   output started;
+   
+   input aresetn;
+
+
+  //wire [clogb2(RAM_DEPTH-1)-1:0] addra;  // Address bus, width determined from RAM_DEPTH
+  wire [14:0] addra;
+  wire [RAM_WIDTH-1:0] dina;           // RAM input data
+  wire clka;                           // Clock
+  wire [3:0] wea;
+  //wire wea;                            // Write enable
+  wire ena;                            // RAM Enable, for additional power savings, disable port when not in use
+  wire rsta;                           // Output reset (does not affect memory contents)
+  wire regcea;                         // Output register enable
+  wire [RAM_WIDTH-1:0] douta;                   // RAM output data
+
+  reg [RAM_WIDTH-1:0] myRam [RAM_DEPTH-1:0];
+  reg [RAM_WIDTH-1:0] ramData = {RAM_WIDTH{1'b0}};
+  
+  reg started;
+
+  // The following code either initializes the memory values to a specified file or to all zeros to match hardware
+  generate
+    if (INIT_FILE != "") begin: use_init_file
+      initial
+        $readmemh(INIT_FILE, myRam, 0, RAM_DEPTH-1);
+    end else begin: init_bram_to_zero
+      integer ram_index;
+      initial
+        for (ram_index = 0; ram_index < RAM_DEPTH; ram_index = ram_index + 1)
+          myRam[ram_index] = {RAM_WIDTH{1'b0}};
+    end
+  endgenerate
+/*
+  always @(negedge aresetn, posedge start) begin
+    if (aresetn) begin
+        if (start) begin
+            started<=1;
+            end
+    end else begin
+        started<=0;
+        end    
+  end*/
+
+  always @(posedge clka)
+    if (ena)
+      if (wea[0])
+        myRam[addra] <= dina;
+      else
+        ramData <= myRam[addra];
+
+  //  The following code generates HIGH_PERFORMANCE (use output register) or LOW_LATENCY (no output register)
+  generate
+    if (RAM_PERFORMANCE == "LOW_LATENCY") begin: no_output_register
+
+      // The following is a 1 clock cycle read latency at the cost of a longer clock-to-out timing
+       assign douta = ramData;
+
+    end else begin: output_register
+
+      // The following is a 2 clock cycle read latency with improve clock-to-out timing
+
+      reg [RAM_WIDTH-1:0] douta_reg = {RAM_WIDTH{1'b0}};
+
+      always @(posedge clka)
+        if (rsta)
+          douta_reg <= {RAM_WIDTH{1'b0}};
+        else if (regcea)
+          douta_reg <= ramData;
+
+      assign douta = douta_reg;
+
+    end
+  endgenerate
+  
+  
+endmodule
